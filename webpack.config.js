12f287920fc1e5ca2f62c8a889955884cd5231de
DieGomezR <129317836+DieGomezR@users.noreply.github.com>
2025-01-17 13:14:39 -0500

    Folder structure for header and top-bar.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
-
 const path = require("path");
-
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./src/app.js",
   output: {
     path: path.resolve(__dirname, "public"),
