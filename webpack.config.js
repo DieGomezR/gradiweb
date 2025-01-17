93ab16cdb1808dd819403fbb3ee99ae6d8cb26e4
DieGomezR <129317836+DieGomezR@users.noreply.github.com>
2025-01-17 08:35:44 -0500

    add gitignore and config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,39 @@
-const path = require('path');
+
+const path = require("path");
+
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/app.js',
+  entry: "./src/app.js",
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'main.js',
+    path: path.resolve(__dirname, "public"),
+    filename: "main.js",
+    clean: true,
+    assetModuleFilename: "assets/[name][ext]",
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|avif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/[name][ext]",
+        },
       },
     ],
   },
-  mode: 'development',
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "assets",
+          to: "assets",
+        },
+      ],
+    }),
+  ],
 };
